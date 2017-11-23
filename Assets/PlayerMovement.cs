339d79236b5e428bb31ce72714926ed63fa90921
KAMIT\lauralai <lauralaine@kamk.fi>
2017-11-23 19:17:04 +0200

    Player blocks created and movement script started.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody2D rig;
+    public float speed = 10.0F;
+
+    // Use this for initialization
+    void Awake () {
+        rig = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Move () {
+
+        float move = Input.GetAxis("Horizontal");
+        rig.AddForce(new Vector2(move, 0), ForceMode2D.Force);
+	}
+
+    void Jump ()
+    {
+    }
+    void Rotate ()
+    {
+
+    }
+}
