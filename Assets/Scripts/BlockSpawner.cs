5c35f04ea96562eb61dce9ccba65f4c58d61478c
KAMIT\lauralai <lauralaine@kamk.fi>
2017-11-23 16:36:22 +0200

    Z and L block (added) fall.

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    public GameObject[] fallingBlocks;
+    public float spawnSpeed;
+
+    public float MinX;
+    public float MaxX;
+
+    public void Start()
+    {
+        InvokeRepeating("Spawn", 0, spawnSpeed);
+    }
+
+    public void Spawn()
+    {
+        float x = Random.Range(MinX, MaxX);
+        
+        GameObject blockPrefab = fallingBlocks[Random.Range(0, fallingBlocks.Length)];
+        Instantiate(blockPrefab, new Vector2(x, 60), Quaternion.identity);
+    }
+
+    public int getDegree()
+    {
+         
+        int degree = Random.Range(0, 3);
+
+        return degree;
+    }
+}
